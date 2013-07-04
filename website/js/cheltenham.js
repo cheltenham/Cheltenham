ab77d0760d5a272a2f8724e4d2c07aa7b87ce9e7
Keith Policano <kpolicano@gmail.com>
2013-07-04 16:19:15 -0400

    adding directory

--- website/js/cheltenham.js
+++ website/js/cheltenham.js
@@ -9,3 +9,23 @@ function styleTableRows() {
 	$("tr:even").addClass("tr-even");
 	$("tr:odd").addClass("tr-odd");
 }
+
+function validateUsername(username) {
+	var salt = 'BMiT!V)R++e';
+	var hash = '5f1f7e109c39d4a0691fec571d6d84c679294a655d9dd6293d67f6d511063744';
+	return hash == CryptoJS.SHA256(username + salt).toString();
+}
+
+function validatePassword(password) {
+	var salt = 'ZcEh*3-/{';
+	var hash = '082b9b907c1beb8752e2e5718a50fb3ac5d648b50fb95aa0424e5a9aa8d4f822';
+	return hash == CryptoJS.SHA256(password + salt).toString();
+}
+
+function fetchDirectory(username, password) {
+	if (!validateUsername(username) || !validatePassword(password)) {
+		alert("Incorrect username or password.");
+	} else {
+		window.open('doc/directory.pdf', '_blank')
+	}
+}
